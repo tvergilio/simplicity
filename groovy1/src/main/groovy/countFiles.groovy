b3576c0c3ff89c5a156f0225f5ad45cd691a498d
Peter Ledbrook <peter@cacoethes.co.uk>
2015-09-14 07:15:41 +0100

    Add file counting script exercise.

---
+++ groovy1/src/main/groovy/countFiles.groovy
@@ -0,0 +1,16 @@
+/**
+ * TODO #36: (Advanced) Write a script that counts all the files in a target
+ * directory that have a given suffix. The script should recurse into
+ * subdirectories when counting. You should be able to launch the script with
+ * the command line:
+ *
+ *     groovy countFiles.groovy <suffix>
+ *
+ * For a bit more of a challenge, you can support command line options that
+ * change the criteria for the search. For example, --biggerthan <bytesize>
+ * would find files larger than the given size.
+ *
+ * Don't forget that you can access the command line arguments from the
+ * implicit script variabls `args`. This is an array of strings representing
+ * THe arguments that come after the script file name.
+ */
