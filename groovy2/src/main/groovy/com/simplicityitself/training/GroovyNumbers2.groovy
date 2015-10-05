aef61f21edeed2c51f1de895096794dbb530cfba
vergil01 <t.vergilio@leedsbeckett.ac.uk>
2015-10-05 20:57:35 +0100

    Completed GroovyNumbers2 exercises.

---
+++ groovy2/src/main/groovy/com/simplicityitself/training/GroovyNumbers2.groovy
@@ -0,0 +1,31 @@
+package com.simplicityitself.training
+
+/**
+ * Created by Thalita on 05/10/2015.
+ */
+class GroovyNumbers2 {
+
+    def getPositiveNumbers(List<Integer> numberList) {
+        return numberList?.findAll { it > 0 }
+    }
+
+    def getMagnitudeGreaterThanTen(List<Integer> numberList) {
+        return numberList?.findAll { it.abs() > 10 }
+    }
+
+    def getFirstMagnitudeGreaterThanTen(List<Integer> numberList) {
+        return numberList?.find { it.abs() > 10 }
+    }
+
+    def getSquares(List<Integer> numberList) {
+        return numberList?.collect { it**2 }
+    }
+
+    def getCountMagnitudeGreaterThanTen(List<Integer> numberList) {
+        return numberList?.count { it.abs() > 10 }
+    }
+
+    def getEvenNumbersBetweenZeroAndTwenty() {
+        return (0..20).count { it % 2 == 0 }
+    }
+}
