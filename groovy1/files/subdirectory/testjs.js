a1a93843fb1a75d901f38bfd4f42065e9ddd691d
vergil01 <t.vergilio@leedsbeckett.ac.uk>
2015-10-05 19:27:49 +0100

    Re-added exercises from first iteration

---
+++ groovy1/files/subdirectory/testjs.js
@@ -0,0 +1,3 @@
+/**
+ * Created by vergil01 on 21/09/2015.
+ */
--- groovy1/src/main/groovy/com/simplicityitself/training/GroovyCommonsCodec.groovy
+++ groovy1/src/main/groovy/com/simplicityitself/training/GroovyCommonsCodec.groovy
@@ -1,5 +1,7 @@
 package com.simplicityitself.training
 
+import org.apache.commons.codec.digest.DigestUtils
+
 /**
  * <p>You will inevitably need to use third-party libraries at some point, i.e.
  * classes and APIs that don't come as part of the JDK or Groovy. In these
@@ -33,7 +35,7 @@ class GroovyCommonsCodec {
      * a string, as opposed to a byte array.</p>
      */
     String toMD5(String data) {
-        return ""
+        return DigestUtils.md5Hex(data)
     }
 
     /**
@@ -41,6 +43,6 @@ class GroovyCommonsCodec {
      * similar to the previous exercise.</p>
      */
     String toMD5(byte[] data) {
-        return ""
+        return DigestUtils.md5Hex(data)
     }
 }
