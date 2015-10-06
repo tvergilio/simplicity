d3b496058005c22aa91b3c4177c38054a74c6f7e
vergil01 <t.vergilio@leedsbeckett.ac.uk>
2015-10-06 23:38:18 +0100

    Refactored some of the answers following feedback from Peter.

--- groovy2/src/main/groovy/com/simplicityitself/training/GroovyFiles2.groovy
+++ groovy2/src/main/groovy/com/simplicityitself/training/GroovyFiles2.groovy
@@ -12,6 +12,6 @@ public class GroovyFiles2 {
     }
 
     def writeBytes(String path, byte[] bytes) {
-        new File(path)?.withDataOutputStream { it?.write(bytes) }
+        new File(path)?.withOutputStream { it?.write(bytes) }
     }
 }
