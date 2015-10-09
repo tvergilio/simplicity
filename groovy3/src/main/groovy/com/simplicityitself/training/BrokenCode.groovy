2acc3d28a896f796b5cad2d77f7e301032f33489
vergil01 <t.vergilio@leedsbeckett.ac.uk>
2015-10-10 00:28:13 +0100

    BrokenCode now compiles - do I need to write tests?

--- groovy3/src/main/groovy/com/simplicityitself/training/BrokenCode.groovy
+++ groovy3/src/main/groovy/com/simplicityitself/training/BrokenCode.groovy
@@ -2,18 +2,20 @@ package com.simplicityitself.training
 
 import groovy.transform.TypeChecked
 
+import java.math.RoundingMode
+
 /**
  * <p>TODO #4: This class contains several errors that can be picked up by
  * static type checking. Uncomment the {@code @TypeChecked} annotation and then
  * fix the errors. Note that parameterised types usually need to be explicitly
  * declared as such.</p>
  */
-//@TypeChecked
+@TypeChecked
 class BrokenCode {
 
-    int sumNumbers(numbers) {
+    int sumNumbers(Collection<Integer> numbers) {
         def result = 0
-        for (int i in number) {
+        for (int i in numbers) {
             result += i
         }
         return result
@@ -24,25 +26,24 @@ class BrokenCode {
     }
 
     def mean(Collection<Integer> numbers) {
-        return numbers.sum() / numbers.size()
+        return ((Integer) numbers.sum()) / numbers.size()
     }
 
     BigDecimal median(Collection<Integer> numbers) {
         if (numbers.size() % 2 == 0) {
             // Even number of values
             def firstIndex = Math.floorDiv(numbers.size(), 2) - 1
-            return mean(numbers.sort()[firstIndex..(firstIndex + 1)])
-        }
-        else {
+            return (BigDecimal) mean(numbers.sort()[firstIndex..(firstIndex + 1)])
+        } else {
             def index = Math.floorDiv(numbers.size(), 2)
             return numbers.sort().get(index)
         }
     }
 
-    List reverseStrings(Collection strings) {
+    List<String> reverseStrings(Collection<String> strings) {
         def result = []
         for (str in strings) {
-            result << str.revers()
+            result << str.reverse()
         }
         return result
     }
