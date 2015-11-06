210345bd0c2cbabb6528c9d80f05da289cfb643a
vergil01 <t.vergilio@leedsbeckett.ac.uk>
2015-11-06 09:57:17 +0000

    Added unit test for show() action in StudentController

--- grails1/test/unit/org/example/StudentControllerSpec.groovy
+++ grails1/test/unit/org/example/StudentControllerSpec.groovy
@@ -1,20 +1,34 @@
 package org.example
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
+@Mock([Student, Course])
 @TestFor(StudentController)
 class StudentControllerSpec extends Specification {
 
-    def setup() {
-    }
+    void "test show() returns the expected Student"() {
+        given: "an existing Student with Courses"
+        def student = new Student(name: 'Carolina Vergilio', email: 'cvergilio@email.com').save()
+        def course1 = new Course(code: "WAT1", title: "Introduction to Watercolours", description: "An introduction to painting with watercolours.")
+        def course2 = new Course(code: "SCUL", title: "Ceramic Sculpture", description: "A practical approach to producing ceramic artwork.")
+        student.addToCourses(course1)
+        student.addToCourses(course2)
+        student.save(flush: true)
 
-    def cleanup() {
-    }
+        when: "the show() method is called on the controller passing the id of the expected Student"
+        def model = controller.show(student.id)
 
-    void "test something"() {
+        then: "the expected Student is returned, with its associated Courses"
+        model.student.id == student.id
+        model.student.name == student.name
+        model.student.email == student.email
+        model.student.courses.size() == 2
+        model.student.courses.contains(course1)
+        model.student.courses.contains(course2)
     }
 }
