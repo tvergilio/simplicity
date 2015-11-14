4b721bff7074a642fc282efbe96db2678d6aaa15
vergil01 <t.vergilio@leedsbeckett.ac.uk>
2015-11-14 16:13:27 +0000

    Using a private inner class + closure.setDelegate() to test that the closure is doing what it should. Works like a charm.

--- grails1/test/unit/org/example/AskAQuestionServiceSpec.groovy
+++ grails1/test/unit/org/example/AskAQuestionServiceSpec.groovy
@@ -13,7 +13,18 @@ import spock.lang.Specification
 class AskAQuestionServiceSpec extends Specification {
 
     void "test contactHead"() {
-        given: "The details for an e-mail message"
+        given: "A mock implementation for the MailService's sendMail method"
+        def mailDomain = new MailDomain()
+        def mailServiceMock = Mock(MailService)
+        mailServiceMock.sendMail(_) >> { args ->
+            def closure = args[0]
+            closure.setDelegate(mailDomain)
+            closure.call()
+            return null
+        }
+        service.mailService = mailServiceMock
+
+        and: "The details for an e-mail message"
         def messageDetails = [toEmail: 'tvergilio@gmail.com', fromEmail: 't.vergilio@leedsbeckett.ac.uk', body: 'Is the tutor for this course any good?']
 
         and: "An existing Course" //although does it matter whether the Course exists? I don't think it does for this test...
@@ -22,20 +33,37 @@ class AskAQuestionServiceSpec extends Specification {
         faculty.addToCourses(course)
         faculty.save()
 
-        and: "A mock implementation for the AskAQuestionService's MailService"
-        def mailService = Mock(MailService)
-        service.mailService = mailService
-
         when: "The askAQuestion() action is called on the service"
         service.askAQuestion(messageDetails.toEmail, messageDetails.fromEmail, course.title, messageDetails.body)
 
-        then: "The mailService's sendMail method is called once and only once with the correct arguments"
-        1 * mailService.sendMail({ it instanceof Closure }) >> { arguments ->
-            def properties = arguments[0].getProperties()
-            assert properties.sendTo == messageDetails.toEmail
-            assert properties.sentFrom == messageDetails.fromEmail
-            assert properties.emailSubject == course.title
-            assert properties.emailBody == messageDetails.body
+        then: "The mailService's sendMail method is called once and only once and the arguments passed by the closure are correct"
+        1 * mailServiceMock.sendMail(_)
+        assert mailDomain.to == properties.sendTo
+        assert mailDomain.from == properties.sentFrom
+        assert mailDomain.subject == properties.emailSubject
+        assert mailDomain.body == properties.emailBody
+    }
+
+    private class MailDomain {
+        public to
+        public from
+        public subject
+        public body
+
+        public to(String to) {
+            to = to
+        }
+
+        public from(String from) {
+            from = from
+        }
+
+        public subject(String subject) {
+            subject = subject
+        }
+
+        public body(String body) {
+            body = body
         }
     }
 }
